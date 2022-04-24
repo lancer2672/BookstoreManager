3e6844c6efc729f32b2f2c69b4b72d60d9035378
Khanh <jbkhanhtran@gmail.com>
2022-04-24 21:43:46 +0700

    them Menu cho MainWindow

--- BookstoreManager/MainWindow.xaml.cs
+++ BookstoreManager/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookstoreManager.ViewModels;
+using BookstoreManager.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- BookstoreManager/ViewModels/MainViewModel.cs
+++ BookstoreManager/ViewModels/MainViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace BookstoreManager.ViewModels
 {
     public class MainViewModel:BaseViewModel
     {
+        public bool IsLoaded;
+        public ICommand LoadedWidnowCommand { get; set; }
+        public MainViewModel()
+        {
+            IsLoaded = false;
+        }
     }
 }
