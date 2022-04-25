1900f86a4e8fc292c315655c9abeb802feb2ef5c
Khanh <jbkhanhtran@gmail.com>
2022-04-25 17:25:55 +0700

    Them datacontext cho mainwindow

--- BookstoreManager/ViewModels/MainViewModel.cs
+++ BookstoreManager/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookstoreManager.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,18 @@ namespace BookstoreManager.ViewModels
 {
     public class MainViewModel:BaseViewModel
     {
-        public bool IsLoaded;
+        public bool IsLoaded = false;
         public ICommand LoadedWidnowCommand { get; set; }
         public MainViewModel()
         {
-            IsLoaded = false;
+            LoadedWidnowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                IsLoaded = true;
+                p.Hide();
+                AdminWindow admWindow = new AdminWindow();
+                admWindow.ShowDialog();
+            });
+
         }
     }
 }
