63776febf37312414ae9ae1c7960e88780ddf759
Khanh <jbkhanhtran@gmail.com>
2022-04-27 20:45:50 +0700

    Them file class ViewCustomer

---
+++ BookstoreManager/Models/ViewCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreManager.Models
+{
+    public class ViewCustomer
+    {
+        public string TenKhachHang { get;set; }
+        public string DiaChi    { get;set; }
+        public string SDT   { get;set; }
+
+    }
+}
