7ce39e46b91f1937dce5e4b23e09952f51ce69bd
Khanh <jbkhanhtran@gmail.com>
2022-04-27 10:27:40 +0700

    them file Pages.cs

---
+++ BookstoreManager/Views/Pages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BookstoreManager.Views
+{
+    static class Pages
+    {
+        public static Page ManageCustomerPagea { get => ListPages[1]; }
+
+        public static List<Page> ListPages = new List<Page>();
+        public static Page ManageCustomerPage { get => ListPages[0]; }
+        static Pages()
+        {
+            ListPages.Add(new ManageCustomerPage());
+        }
+    }
+}
