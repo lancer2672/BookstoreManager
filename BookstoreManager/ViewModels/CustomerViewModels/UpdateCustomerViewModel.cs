2ebf17f0701e838392cffcb585abd6d787bb60a7
Khanh <jbkhanhtran@gmail.com>
2022-05-13 01:10:15 +0700

    Them phan xac thuc Rule trong UpdateCustomer

--- BookstoreManager/ViewModels/CustomerViewModels/UpdateCustomerViewModel.cs
+++ BookstoreManager/ViewModels/CustomerViewModels/UpdateCustomerViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using BookstoreManager.Models;
 using BookstoreManager.Models.Db;
+using BookstoreManager.Resources.Utils;
 using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace BookstoreManager.ViewModels.Customers
@@ -36,7 +38,7 @@ namespace BookstoreManager.ViewModels.Customers
         {
             _customerViewModel = CustomerVM;
             LoadUpdWindow();
-            CUpdateCustomer = new RelayCommand<object>((p) => { return true; }, (p) => { UpdateCustomer(); });
+            CUpdateCustomer = new RelayCommand<StackPanel>((p) => { return true; }, (p) => { UpdateCustomer(p); });
 
             MyMessageQueue = new SnackbarMessageQueue(TimeSpan.FromMilliseconds(4000));
             MyMessageQueue.DiscardDuplicates = true;
@@ -51,21 +53,37 @@ namespace BookstoreManager.ViewModels.Customers
             CustomerDebt = updCustomer.Debt.ToString();
 
         }
-        public void UpdateCustomer()
-        {
-            ViewCustomer customer = _customerViewModel.SelectedCustomer;
-            KHACHHANG updCustomer = DataProvider.Ins.DB.KHACHHANGs.Where(p => p.MaKhachHang == customer.Id).FirstOrDefault();
-            updCustomer.HoTen = CustomerName;
-            updCustomer.DiaChi = CustomerAddress;
-            updCustomer.DienThoai = customer.PhoneNumber;
-            updCustomer.Email = customer.Email;
-            updCustomer.TongNo = (decimal)customer.Debt;
-            DataProvider.Ins.DB.SaveChanges();
-            _customerViewModel.LoadListCustomer();
-            RefreshAddCustomerForm();
-            MyMessageQueue.Enqueue("Chỉnh sửa khách hàng thành công!");
+        public void UpdateCustomer(StackPanel t)
+        {         
+            if (Validator.IsValid(t))
+            {
+                ViewCustomer customer = _customerViewModel.SelectedCustomer;
+                KHACHHANG updCustomer = DataProvider.Ins.DB.KHACHHANGs.Where(p => p.MaKhachHang == customer.Id).FirstOrDefault();
+                updCustomer.HoTen = CustomerName;
+                updCustomer.DiaChi = CustomerAddress;
+                updCustomer.DienThoai = customer.PhoneNumber;
+                updCustomer.Email = customer.Email;
+                updCustomer.TongNo = (decimal)customer.Debt;
+                MyMessageQueue.Enqueue("Chỉnh sửa khách hàng thành công!");
+                try
+                {
+                    DataProvider.Ins.DB.SaveChanges();
+                }
+                catch
+                {
+                    _customerViewModel.MyMessageQueue.Enqueue("Lỗi. Thông tin khách hàng không hợp lệ");
+                    return;
+                }
+                _customerViewModel.LoadListCustomer();
+                RefreshUpdateCustomerForm();
+                _customerViewModel.MyMessageQueue.Enqueue("Chỉnh sửa thông tin khách hàng thành công!");
+            }
+            else
+            {
+                _customerViewModel.MyMessageQueue.Enqueue("Lỗi. Thông tin khách hàng không hợp lệ");
+            }
         }
-        public void RefreshAddCustomerForm()
+        public void RefreshUpdateCustomerForm()
         {
             CustomerName = "";
             CustomerAddress = "";
