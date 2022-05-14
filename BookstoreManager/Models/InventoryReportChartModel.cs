72ce474963f6d005612861c9f96b329f3a58ec2b
Khanh <jbkhanhtran@gmail.com>
2022-05-14 16:43:41 +0700

    add class InventoryReportChartModel

---
+++ BookstoreManager/Models/InventoryReportChartModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreManager.Models
+{
+    public class InventoryReportChartModel
+    {
+        public int Value { get; set; }
+        public string Month { get; set; }
+        public InventoryReportChartModel(int X, int Y)
+        {
+            Value = X;
+            Month = "Tháng " + Y.ToString();
+        }
+    }
+}
