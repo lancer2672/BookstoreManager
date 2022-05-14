d72da5b7054e2f3c64597154296807163c902824
Khanh <jbkhanhtran@gmail.com>
2022-05-14 14:36:38 +0700

    chinh sua logic

--- BookstoreManager/ViewModels/ReportAndStatistic/InventoryReportViewModel.cs
+++ BookstoreManager/ViewModels/ReportAndStatistic/InventoryReportViewModel.cs
@@ -14,7 +14,11 @@ namespace BookstoreManager.ViewModels.ReportAndStatistic
     {
         private ObservableCollection<InventoryReportItem> _dataListView;
         private List<int> _listYear;
+        private List<int> _listMonth = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
         private int _selectedYear;
+        private int _selectedMonth;
+        public List<int> ListMonth { get => _listMonth; }
+        public int SelectedMonth { get { return _selectedMonth; } set { _selectedMonth = value; OnPropertyChanged(nameof(SelectedMonth)); } }
         public int SelectedYear { get { return _selectedYear; } set { _selectedYear = value; OnPropertyChanged(nameof(SelectedYear)); } }
         public List<int> ListYear { get => _listYear; set { _listYear = value; OnPropertyChanged(nameof(ListYear)); } }
         public ObservableCollection<InventoryReportItem> DataListView { get { return _dataListView; } set { _dataListView = value; OnPropertyChanged(nameof(DataListView)); } }
@@ -45,26 +49,25 @@ namespace BookstoreManager.ViewModels.ReportAndStatistic
         }
         public void LoadDataListView()
         {
-            List<SACH> BookList = DataProvider.Ins.DB.SACHes.ToList();
-            DataListView = GetDataListViewFromDB(BookList);
+                BAOCAOTON InvReport = DataProvider.Ins.DB.BAOCAOTONs.Where(p => p.Thang == SelectedMonth && p.Nam == SelectedYear).FirstOrDefault();
+                if (InvReport != null)
+                { 
+                    DataListView = GetDataListViewFromDB(InvReport);
+                }
         }
-        public ObservableCollection<InventoryReportItem> GetDataListViewFromDB(List<SACH> BookList)
+        public ObservableCollection<InventoryReportItem> GetDataListViewFromDB(BAOCAOTON InvReport)
         {
             ObservableCollection<InventoryReportItem> Data = new ObservableCollection<InventoryReportItem>();
+            List<SACH> BookList = DataProvider.Ins.DB.SACHes.ToList();
             foreach (SACH book in BookList)
             {
                 InventoryReportItem Item = new InventoryReportItem();
                 THELOAI Type = DataProvider.Ins.DB.THELOAIs.Where(p => p.MaTheLoai == book.MaTheLoai).FirstOrDefault();
-                if (Type == null)
-                {
-                    Item.Type = "";
-                }
-                else
-                { 
-                    Item.Type = Type.TenTheLoai; 
-                }
+                Item.Type = Type.TenTheLoai; 
                 Item.Id = book.MaSach;
-                Item.Quantity = (int)book.SoLuongTon;
+                Item.FirstQuantity = (int)InvReport.TonDau;
+                Item.IncurredQuantity = (int)InvReport.PhatSinh;
+                Item.EndQuantity = (int)InvReport.TonCuoi;
                 Item.PublishingHouse = book.NhaXuatBan;
                 Item.PublishYear = (int)book.NamXuatBan;
             }
