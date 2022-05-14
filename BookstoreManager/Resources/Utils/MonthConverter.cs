9839f7c7032b80cd2a4a1d42d3e052d3c86ae99c
Khanh <jbkhanhtran@gmail.com>
2022-05-14 14:34:56 +0700

    them MonthConverter

---
+++ BookstoreManager/Resources/Utils/MonthConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BookstoreManager.Resources.Utils
+{
+    public class MonthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string month = "Tháng " + value;
+            return month;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
