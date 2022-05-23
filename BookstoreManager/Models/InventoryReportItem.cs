0bfbd6ba7f6ca58d9e7bd57cd185c4bc192b61ad
Khanh <jbkhanhtran@gmail.com>
2022-05-24 00:23:24 +0700

    them chuc nang Import Export file cho InvReport

--- BookstoreManager/Models/InventoryReportItem.cs
+++ BookstoreManager/Models/InventoryReportItem.cs
@@ -8,14 +8,18 @@ namespace BookstoreManager.Models
 {
     public class InventoryReportItem
     {
-        public int Id { get; set; }
+        public int BookId { get; set; }
         public string BookName { get; set; }
-
         public string Type { get; set; }
         public int FirstQuantity { get; set; }
         public int IncurredQuantity { get; set; }
         public int EndQuantity { get; set; }
-      
+
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+
+
 
     }
 }
