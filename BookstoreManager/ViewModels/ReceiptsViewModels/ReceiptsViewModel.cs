d3906fb6ab219f60f2c63c527e2c1e53bc1f6fb5
20520846 <90497301+20520846@users.noreply.github.com>
2022-05-26 18:47:17 +0700

    phieu' thu

---
+++ BookstoreManager/ViewModels/ReceiptsViewModels/ReceiptsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BookstoreManager.ViewModels.ReceiptsViewModels
+{
+    public class ReceiptsViewModel : BaseViewModel
+    {
+        private int _receiptsId;
+        private int _customerId;
+        private DateTime _date;
+        private decimal _receiptsMoney;
+        public SnackbarMessageQueue MyMessageQueue { get => myMessageQueue; set { myMessageQueue = value; OnPropertyChanged(nameof(MyMessageQueue)); } }
+        private SnackbarMessageQueue myMessageQueue;
+        public int ReceiptsId { get => _receiptsId; set { _receiptsId = value; OnPropertyChanged(); } }
+        public int CustomerId { get => _customerId; set { _customerId = value; OnPropertyChanged(); } }
+        public DateTime Date { get => _date; set { _date = value; OnPropertyChanged(); } }
+        public decimal ReceiptsMoney{ get => _receiptsMoney; set { _receiptsMoney = value; OnPropertyChanged(); } }
+        public ICommand SaveReceipts { get; set; }
+        public ReceiptsViewModel()
+        {
+
+        }
+
+    }
+}
